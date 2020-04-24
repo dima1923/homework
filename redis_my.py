a83de62f9abcf2072bd7e523811625a59e1b800b
dmitry <dima1923@me.com>
2020-04-24 14:06:30 +0300

    add

--- redis_my.py
+++ redis_my.py
@@ -13,7 +13,7 @@ containers_name = {
 
 os.system('docker-compose up -d')
 sleep(15)
-sentinel = Sentinel([('localhost', 6666)], socket_timeout=0.1)
+sentinel = Sentinel([('localhost', 6666),('localhost', 7777),('localhost', 8888)], socket_timeout=0.1)
 print(sentinel.discover_master('mymaster'))
 master = sentinel.master_for('mymaster')
 my_dict = {'key'+str(i):'value'+str(i) for i in range(0,20)}
@@ -25,7 +25,9 @@ for k in my_dict.keys():
 sleep(3)
 print("выключаем мастера")
 os.system('docker stop %s'%containers_name["master"])
-sleep(30)
+print("выключаем первый сентинел")
+os.system('docker stop %s'%containers_name["sentinel1"])
+sleep(60)
 master1 = sentinel.master_for('mymaster')
 print('новый мастер:'+sentinel.discover_master('mymaster').__str__())
 print("данные с нового мастера")
